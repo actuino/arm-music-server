82bf4c73ea997eee81ac3be7194b63f4d79e157c
SylvainD <syl@sylvain-deaure.fr>
2017-01-30 21:40:30 +0000

    fixed volumes

--- run.sh
+++ run.sh
@@ -3,6 +3,6 @@
 
 docker network create -d bridge arm_music
 
-docker run -d -p 6600:6600 --net arm_music -v /var/lib/mpd:/var/lib/mpd --name mpd actuino/mpd-armhf:dev
+docker run -d -p 6600:6600 --net arm_music -v /var/lib/mpd/music:/var/lib/mpd/music -v /var/lib/mpd/playlists:/var/lib/mpd/playlists -v /var/lib/mpd/database:/var/lib/mpd/database --name mpd actuino/mpd-armhf:dev
 
 docker run -d -p 8080:8080 --net arm_music --name=ympd -d actuino/ympd-armhf:dev
