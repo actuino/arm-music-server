660f65ddaf50d9364c242caa50c419263d74cf5e
SylvainD <syl@sylvain-deaure.fr>
2017-01-30 20:11:12 +0000

    added start script

---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+docker network create -d bridge arm_music
+
+docker run -d -p 6600:6600 --net music_stack -v /var/lib/mpd:/var/lib/mpd --name mpd actuino/mpd-armhf:dev
+
+docker run -d -p 8080:8080 --net music_stack --name=ympd -d actuino/ympd-armhf:dev
