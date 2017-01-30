d65383ad264d0b472034ff0914f477ceb98cd3c6
SylvainD <syl@sylvain-deaure.fr>
2017-01-30 20:40:20 +0000

    fixed mpd.conf

--- run.sh
+++ run.sh
@@ -3,6 +3,6 @@
 
 docker network create -d bridge arm_music
 
-docker run -d -p 6600:6600 --net music_stack -v /var/lib/mpd:/var/lib/mpd --name mpd actuino/mpd-armhf:dev
+docker run -d -p 6600:6600 --net music_stack -v /var/lib/mpd:/var/lib/mpd--name mpd actuino/mpd-armhf:dev
 
 docker run -d -p 8080:8080 --net music_stack --name=ympd -d actuino/ympd-armhf:dev
